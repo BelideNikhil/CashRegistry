00154a0342e2a7a5f00f9dcd73094b392cb8036b
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-07-17 22:50:41 +0530

    refactor style-display

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ let tenn=document.querySelector("#ten")
 let fivee=document.querySelector("#five")
 let onee=document.querySelector("#one")
 
-const output_data=document.querySelector(".whole_data")
+const whole_data=document.querySelector(".whole_data")
+const output_div=document.querySelector(".output")
 
 const Notes=[two_thousand,five_hundred,hundredd,tenn,fivee,onee]
 const notes_num= [2000, 500, 100, 10, 5, 1]
 
 
 calculate.addEventListener("click",()=>{
+    output_div.style.display="block";
     let bill_amount=document.querySelector("#bill")
     let paid_amount=document.querySelector("#paid")
     const calculate=document.querySelector("#calculate")
@@ -22,8 +24,9 @@ calculate.addEventListener("click",()=>{
 
     paid_amount=Number(paid_amount.value)
     bill_amount=Number(bill_amount.value)
-    change.innerHTML=paid_amount-bill_amount
-    returnable=change.innerHTML
+    change.innerHTML=`Please return <i class="fas fa-rupee-sign"></i>  ${paid_amount-bill_amount} to the customer.`
+    change.style.color='green';
+    returnable=paid_amount-bill_amount
     if( bill_amount===0 || paid_amount===0 || returnable<0 ){
         errorhandler();
     }
@@ -33,8 +36,7 @@ calculate.addEventListener("click",()=>{
 })
 
 function handler(){
-    output_data.style.display="block";
-
+    whole_data.style.display="block";
     for(let i=0;i<notes_num.length;i++){
         Notes[i].innerHTML=Math.floor(returnable/notes_num[i])
         newRemainder=(returnable%notes_num[i]);
@@ -44,12 +46,13 @@ function handler(){
 function errorhandler(){
     change.innerHTML="Please enter valid data";
     change.style.color="pink";
-    output_data.style.display="none";
-
+    whole_data.style.display="none";
+    output_div.style.display="block";
 
     setTimeout(()=>{
         change.innerHTML=""
         change.style.color="white";
+        output_div.style.display="none";
     },2500)
 }
 clear.addEventListener("click",()=>{
